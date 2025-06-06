33096a0bb95b6be2ca1834366a83e4a2bdaac7d4
alduris <152772325+alduris@users.noreply.github.com>
2025-06-06 19:01:52 -0400

    The whole thing

---
+++ src/Options.cs
@@ -0,0 +1,25 @@
+﻿using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace DropwigRandomizer
+{
+    internal class Options : OptionInterface
+    {
+        internal static Configurable<int> DropwigCount;
+
+        public Options()
+        {
+            DropwigCount = config.Bind("dropwig_count", 50, new ConfigAcceptableRange<int>(1, 999));
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Tabs = [new OpTab(this)];
+
+            var dragger = new OpDragger(DropwigCount, new Vector2(300f - 12f, 300f - 12f));
+            Tabs[0].AddItems(dragger);
+        }
+    }
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,15 +1,19 @@
-﻿using BepInEx;
-using BepInEx.Logging;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Security.Permissions;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using Watcher;
 
 // Allows access to private members
 #pragma warning disable CS0618
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace TestMod;
+namespace DropwigRandomizer;
 
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+[BepInPlugin("alduris.dropwigs", "Dropwig Randomizer", "1.0")]
 sealed class Plugin : BaseUnityPlugin
 {
     public static new ManualLogSource Logger;
@@ -19,6 +23,8 @@ sealed class Plugin : BaseUnityPlugin
     {
         Logger = base.Logger;
         On.RainWorld.OnModsInit += OnModsInit;
+        On.OverWorld.ctor += OverWorld_ctor;
+        On.World.ctor += World_ctor;
     }
 
     private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
@@ -28,7 +34,80 @@ sealed class Plugin : BaseUnityPlugin
         if (IsInit) return;
         IsInit = true;
 
-        // Initialize assets, your mod config, and anything that uses RainWorld here
-        Logger.LogDebug("Hello world!");
+        MachineConnector.SetRegisteredOI("alduris.dropwigs", new Options());
+    }
+
+    internal string selectedRegion = null;
+    internal bool hasSpawned = false;
+
+    private void OverWorld_ctor(On.OverWorld.orig_ctor orig, OverWorld self, RainWorldGame game)
+    {
+        if (game.IsStorySession)
+        {
+            hasSpawned = false;
+            var regions = GetSlugcatRegions(game.StoryCharacter);
+            if (regions.Count > 0)
+                selectedRegion = regions[Random.Range(0, regions.Count)];
+            else
+                selectedRegion = null;
+
+            Plugin.Logger.LogDebug("Selected region: " + selectedRegion ?? "<NULL>");
+        }
+
+        orig(self, game);
+
+        static List<string> GetSlugcatRegions(SlugcatStats.Name storyCharacter)
+        {
+            if (ModManager.Watcher && storyCharacter == WatcherEnums.SlugcatStatsName.Watcher)
+            {
+                return [
+                    "WBLA", "WPTA", "WRRA", "WVWA",
+                    "WTDA", "WTDB", "WRFA", "WRFB", "WSKA", "WSKB", "WSKC", "WSKD",
+                    "WARA", "WARB", "WARC", "WARD", "WARE", "WARF", "WARG",
+                    "WAUA", "WRSA",
+                    "WSSR", "WSUR", "WHIR", "WDSR", "WGWR", "WORA"];
+            }
+            return [.. SlugcatStats.SlugcatStoryRegions(storyCharacter).Union(SlugcatStats.SlugcatOptionalRegions(storyCharacter))];
+        }
+    }
+
+    private void World_ctor(On.World.orig_ctor orig, World self, RainWorldGame game, Region region, string name, bool singleRoomWorld)
+    {
+        orig(self, game, region, name, singleRoomWorld);
+
+        if (selectedRegion != null && game != null && game.IsStorySession && string.Equals(name, selectedRegion, System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            self.worldProcesses.Add(new DropwigSpawner(self, this));
+        }
+    }
+
+    private class DropwigSpawner(World world, Plugin plugin) : World.WorldProcess(world)
+    {
+        private readonly Plugin plugin = plugin;
+
+        public override void Update()
+        {
+            base.Update();
+            if (!plugin.hasSpawned)
+            {
+                var rooms = world.abstractRooms.Where(room => room.nodes.Any(node => node.type == AbstractRoomNode.Type.Den)).ToArray();
+                if (rooms.Length > 0)
+                {
+                    var room = rooms[Random.Range(0, rooms.Length)];
+                    Plugin.Logger.LogDebug("Room spawned in: " + room.name);
+                    var possibleNodes = room.nodes.Select((node, i) => node.type == AbstractRoomNode.Type.Den ? i : -1).Where(x => x > -1).ToArray();
+                    int toSpawn = Options.DropwigCount.Value;
+                    for (int i = 0; i < toSpawn; i++)
+                    {
+                        var node = possibleNodes[Random.Range(0, possibleNodes.Length)];
+                        var coord = new WorldCoordinate(room.index, -1, -1, node);
+                        var crit = new AbstractCreature(world, StaticWorld.GetCreatureTemplate(CreatureTemplate.Type.DropBug), null, coord, world.game.GetNewID());
+                        crit.saveCreature = false;
+                        room.AddEntity(crit);
+                    }
+                }
+                plugin.hasSpawned = true;
+            }
+        }
     }
 }
